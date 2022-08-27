3f57d79c0c8ec7094f3bfcd92c27ffac6ed61ffc
nikitakosatka <nikitakosatka@yandex.ru>
2022-08-27 14:20:16 +0300

    Run endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/nikitakosatka/markdown-notes/pkg/config"
+	"github.com/nikitakosatka/markdown-notes/pkg/handler"
+	"log"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config.Connect(&config.DBConfig{
+		Username: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Name:     os.Getenv("DB_NAME"),
+	})
+
+	r := gin.Default()
+	handler.IncludeRouter(r)
+
+	r.Run()
 }
